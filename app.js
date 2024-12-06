f766506c0b922535a2189e9576b5331872d48ab1
Gian David Marquez <giandmarquez@gmail.com>
2024-12-05 23:18:32 -0800

    change hello world

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Imagine if this was a minecraft server!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
